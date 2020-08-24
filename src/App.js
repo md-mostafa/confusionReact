f6cbca63b172d80ac03166657fd3aca6e3a6d0fa
md-mostafa <md.mostafa.akash@gmail.com>
2020-08-24 08:54:59 +0600

    component part 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes';
 
 function App() {
+  const [dishes, setDishes] = useState(DISHES);
   return (
     <div>
       <Navbar dark color="primary">
@@ -11,7 +13,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={dishes}/>
     </div>
   );
 }
