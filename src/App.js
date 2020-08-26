fa7e83832ed4b89163270e4a290da55ee7d3d720
md-mostafa <md.mostafa.akash@gmail.com>
2020-08-27 05:54:46 +0600

    reat-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Main from './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div>
-      <Main/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Main/>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/HomeComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div className="container">
+            <h4>Home</h4>
+            <p>Akash is the best</p>
+        </div>
+    );
+}
+
+export default Home;
