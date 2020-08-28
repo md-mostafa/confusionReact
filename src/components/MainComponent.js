88b8350e838b8c195eb9dc517640a8d9e01d4c13
md-mostafa <md.mostafa.akash@gmail.com>
2020-08-28 06:05:06 +0600

    about us included

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetailComponent from './DishdetailComponent';
@@ -49,6 +50,7 @@ class Main extends Component {
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes} />} />
                     <Route path="/menu/:dishId" component={DishWithId} />
+                    <Route path="/aboutus" component={()=> <About leaders={this.state.leaders} />} />
                     <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
