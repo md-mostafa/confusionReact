b4ab885dfcd98f6a9a0cbd73d33187b95741b586
md-mostafa <md.mostafa.akash@gmail.com>
2020-09-01 08:40:34 +0600

    redux thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,11 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Dishes} from './dishes';
 import {Comments} from './comments';
 import {Promotions} from './promotions';
 import {Leaders} from './leaders';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 
 //createStore takes two parameters Reducers and state
 export const ConfigureStore = () => {
@@ -12,7 +15,8 @@ export const ConfigureStore = () => {
             comments: Comments,
             promotions: Promotions,
             leaders: Leaders
-        })
+        }),
+        applyMiddleware(thunk, logger)
     );
 
     return store;
