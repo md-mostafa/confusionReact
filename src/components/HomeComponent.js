c538a124538f5aa123620634aae00afff7093aa3
md-mostafa <md.mostafa.akash@gmail.com>
2020-09-02 09:07:02 +0600

    react-animation-component

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -3,25 +3,31 @@ import { Loading } from './LoadingComponent';
 import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
 import { baseUrl } from '../shared/baseUrl';
 
+import { FadeTransform } from 'react-animation-components';
+
 function RenderCard({item, isLoading, errMess}) {
     if(isLoading) {
         return (
             <Loading />
-        )
-    }else if(errMess) {
+        );
+    }
+    else if(errMess) {
         return (
             <h4>{errMess}</h4>
         );
-    }else
+    }
+    else
         return (
-            <Card>
-                <CardImg top width="100%" src={baseUrl + item.image} alt={item.name} />
-                <CardBody>
-                    <CardTitle>{item.name}</CardTitle>
-                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
-                    <CardText>{item.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform in transformProps={{exitTransform: 'scale(0.5) translateY(-50%)'}}>
+                <Card>
+                    <CardImg src={baseUrl + item.image} alt={item.name} />
+                    <CardBody>
+                        <CardTitle>{item.name}</CardTitle>
+                        {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                        <CardText>{item.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         );
 }
 
